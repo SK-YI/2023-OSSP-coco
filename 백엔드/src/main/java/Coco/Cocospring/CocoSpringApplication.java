c51abcb140a0167d81785e94993e4dbedc247be7
juhno1023 <hyeondnr1023@gmail.com>
2023-08-10 23:05:39 +0900

    init

---
+++ 백엔드/src/main/java/Coco/Cocospring/CocoSpringApplication.java
@@ -0,0 +1,13 @@
+package Coco.Cocospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocoSpringApplication.class, args);
+	}
+
+}
